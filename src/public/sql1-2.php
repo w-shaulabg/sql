7f30d1a358b68181e57f9a0baf0095ba6effa3cd
w-shaila <w-shaulabg.2952r-3@outlook.jp>
2022-04-22 21:11:17 +0900

    課題sql1-2

---
+++ src/public/sql1-2.php
@@ -0,0 +1,10 @@
+<?php
+$dbUserName = "root";
+$dbPassword = "password";
+$pdo = new PDO("mysql:host=mysql; dbname=tq_quest; charset=utf8", $dbUserName, $dbPassword);
+
+$sql = "SELECT * FROM spendings";
+$statement = $pdo->prepare($sql);
+$statement->execute();
+$ispendings = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>

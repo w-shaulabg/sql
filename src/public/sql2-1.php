5f61952e0d6f35b63053a55ad35d5c92e6b6bd4a
w-shaila <w-shaulabg.2952r-3@outlook.jp>
2022-05-09 21:09:32 +0900

    sql2-1

---
+++ src/public/sql2-1.php
@@ -0,0 +1,20 @@
+<?php
+$dbUserName = "root";
+$dbPassword = "password";
+$pdo = new PDO("mysql:host=mysql; dbname=tq_quest; charset=utf8", $dbUserName, $dbPassword);
+
+$sql = "SELECT amount FROM incomes ";
+$statement = $pdo->prepare($sql);
+$statement->execute();
+$amount = $statement->fetchAll(PDO::FETCH_ASSOC);
+//var_dump($amount);
+//die;
+$sum = 0;
+foreach ($amount as $data) {
+  $sum += $data['amount'];
+}
+
+
+echo "incomesテーブルのamountカラムの合計:" . $sum;
+
+?>

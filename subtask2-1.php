1cfd4db7730e8fb5a71ec213e16eccebca117341
w-shaila <w-shaulabg.2952r-3@outlook.jp>
2022-05-10 21:39:11 +0900

    apple_num

---
+++ subtask2-1.php
@@ -0,0 +1,19 @@
+<?php
+$dbUserName = "root";
+$dbPassword = "password";
+$pdo = new PDO("mysql:host=mysql; dbname=tq_quest; charset=utf8", $dbUserName, $dbPassword);
+
+$sql = "SELECT apple_num FROM sample1";
+$statement = $pdo->prepare($sql);
+$statement->execute();
+$apple_num = $statement->fetchAll(PDO::FETCH_ASSOC);
+//var_dump($apple_num);
+
+//die;
+$sum = 0;
+foreach($apple_num as $data)
+{
+$sum += $data['apple_num'];
+}
+echo $sum;
+?>

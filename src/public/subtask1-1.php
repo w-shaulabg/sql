df7ce1b8e5dc3591e143d3df750236825011ab98
w-shaila <w-shaulabg.2952r-3@outlook.jp>
2022-05-10 20:32:13 +0900

    ロジック初級

---
+++ src/public/subtask1-1.php
@@ -0,0 +1,12 @@
+```php
+<?php
+$dbUserName = "root";
+$dbPassword = "password";
+$pdo = new PDO("mysql:host=mysql; dbname=tq_quest; charset=utf8", $dbUserName, $dbPassword);
+
+$sql = "SELECT * FROM sample1";
+$statement = $pdo->prepare($sql);
+$statement->execute();
+$sample1 = $statement->fetchAll(PDO::FETCH_ASSOC);
+var_dump($sample1)
+?>

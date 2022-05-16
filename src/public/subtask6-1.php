5356f6421a3256a38c3c3898436f0a9545108f37
w-shaila <w-shaulabg.2952r-3@outlook.jp>
2022-05-16 15:28:17 +0900

    課題６

--- src/public/subtask6-1.php
+++ src/public/subtask6-1.php
@@ -9,7 +9,23 @@ $statement->execute();
 $apples = $statement->fetchAll(PDO::FETCH_ASSOC);
 //var_dump($apples);
 
-foreach ($apples as $apple) {
-  
-}
+$apples_1 = ['apple_num'=>12];
+$apples_2 = ['apple_num'=>20];
+$apples_3 = ['apple_num'=>8];
+$apples_4 = ['apple_num'=>35];
 
+var_dump($apples_1);
+
+die;
+$apples_difference = [
+$apples_difference_1 = $apples_1 - $apples_2,
+$apples_difference_2 = $apples_2 - $apples_3,
+$apples_difference_3 = $apples_3 - $apples_4
+];
+
+echo"<pre>";
+var_dump($apples_difference);
+echo"<pre>";
+
+
+?>

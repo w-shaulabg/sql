eebd49dcabc9e6cc5a58a9209d8ae6b6f4df7027
w-shaila <w-shaulabg.2952r-3@outlook.jp>
2022-06-14 21:53:54 +0900

    3.sort問題３、修正

--- src/public/sql3-3.php
+++ src/public/sql3-3.php
@@ -6,30 +6,24 @@ $pdo = new PDO("mysql:host=mysql; dbname=tq_quest; charset=utf8", $dbUserName, $
 $sql = "SELECT * FROM spendings";
 $statement = $pdo->prepare($sql);
 $statement->execute();
-$expense3 = $statement->fetchAll(PDO::FETCH_ASSOC);
+$spendings = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 
-$march = [];
 $amount = [];
-foreach ($expense3 as $expense) {
+foreach ($spendings as $spending) {
 
-  if (preg_match("/-03-/", $expense['accrual_date'])) {
-    $march[] = $expense['name'];
-    $amount[] = $expense['amount'];
+  if (preg_match("/-03-/", $spending['accrual_date'])) {
+    $amount[$spending['name']] = $spending['amount'];
   }
 
 }
 
 echo "3月の支出";
 
-for ($i = 0; $i < count($march); $i++);
-
-for ($i = 0; $i < count($amount); $i++) {
-  
+foreach ($amount as $key => $value) {
   echo "<pre>";
-  echo $march[$i] . ":" . $amount[$i];
+  echo ($key . ":" . $value);
   echo "<pre>";
-  
 }
 
 ?>

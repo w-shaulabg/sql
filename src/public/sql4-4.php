1ccfd229f60c35809c3785eb4a5fb30792cc91a6
w-shaila <w-shaulabg.2952r-3@outlook.jp>
2022-06-28 11:53:44 +0900

    ファイル名修正

--- src/public/sql4-4.php
+++ src/public/sql4-4.php
@@ -8,156 +8,20 @@ $statement = $pdo->prepare($sql);
 $statement->execute();
 $incomes = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-
-$amount_01 = [];
+$amount = [];
+$month = [];
 foreach ($incomes as $income) {
-  if (preg_match('/-01-/', $income['accrual_date'])) {
-    $amount_01[] = $income['amount'];
-  }
+  $date = explode("-", $income['accrual_date']);
+  $month = abs($date[1]);     //絶対値01⇨１
+  $amount[$month] += $income['amount'];   //$amount[1〜12月]　+=　1〜12月該当する支出のそれぞれの合計
 }
 
-$january_amount = array_sum($amount_01);
+echo "月順にsortして月ごとの収入の合計を一覧表示";
 
-
-$amount_02 = [];
-foreach ($incomes as $income) {
-  if (preg_match('/-02-/', $income['accrual_date'])) {
-    $amount_02[] = $income['amount'];
-  }
-}
-
-$february_amount = array_sum($amount_02);
-
-
-$amount_03 = [];
-foreach ($incomes as $income) {
-  if (preg_match('/-03-/', $income['accrual_date'])) {
-    $amount_03[] = $income['amount'];
-  }
-}
-
-$march_amount = array_sum($amount_03);
-
-
-$amount_04 = [];
-foreach ($incomes as $income) {
-  if (preg_match('/-04-/', $income['accrual_date'])) {
-    $amount_04[] = $income['amount'];
-  }
-}
-
-$april_amount = array_sum($amount_04);
-
-
-$amount_05 = [];
-foreach ($incomes as $income) {
-  if (preg_match('/-05-/', $income['accrual_date'])) {
-    $amount_05[] = $income['amount'];
-  }
-}
-
-$may_amount = array_sum($amount_05);
-
-
-$amount_06 = [];
-foreach ($incomes as $income) {
-  if (preg_match('/-06-/', $income['accrual_date'])) {
-    $amount_06[] = $income['amount'];
-  }
-}
-
-$june_amount = array_sum($amount_06);
-
-
-$amount_07 = [];
-foreach ($incomes as $income) {
-  if (preg_match('/-07-/', $income['accrual_date'])) {
-    $amount_07[] = $income['amount'];
-  }
-}
-
-$july_amount = array_sum($amount_07);
-
-
-$amount_08 = [];
-foreach ($incomes as $income) {
-  if (preg_match('/-08-/', $income['accrual_date'])) {
-    $amount_08[] = $income['amount'];
-  }
-}
-
-$august_amount = array_sum($amount_08);
-
-$amount_09 = [];
-foreach ($incomes as $income) {
-  if (preg_match('/-09-/', $income['accrual_date'])) {
-    $amount_09[] = $income['amount'];
-  }
-}
-
-$september_amount = array_sum($amount_09);
-
-
-$amount_10 = [];
-foreach ($incomes as $income) {
-  if (preg_match('/-10-/', $income['accrual_date'])) {
-    $amount_10[] = $income['amount'];
-  }
-}
-
-$october_amount = array_sum($amount_10);
-
-
-$amount_11 = [];
-foreach ($incomes as $income) {
-  if (preg_match('/-11-/', $income['accrual_date'])) {
-    $amount_11[] = $income['amount'];
-  }
-}
-
-$november_amount = array_sum($amount_11);
-
-
-$amount_12 = [];
-foreach ($incomes as $income) {
-  if (preg_match('/-12-/', $income['accrual_date'])) {
-    $amount_12[] = $income['amount'];
-  }
-}
-
-$december_amount = array_sum($amount_12);
-
-
-$years = [];
-$years = ["1月:"=>$june_amount, "2月:"=>$february_amount,"3月:"=>$march_amount,
-"4月:"=>$april_amount,"5月:"=>$may_amount,"6月:"=>$june_amount,"7月:"=>$july_amount,
-"8月:"=>$august_amount,"9月:"=>$september_amount,"10月:"=>$october_amount,
-"11月:"=>$november_amount,"12月:"=>$december_amount];
-
-echo "月順にsortして月ごとの収入の合計を一覧表示" . "\n";
-
-foreach ($years as $key => $value) {
+foreach ($amount as $mon => $total_amount) {   //キー(1〜１２月) =>　支出の合計　連想配列として結び付ける
   echo "<pre>";
-  echo ($key . $value);
+  echo ($mon. "月:" . $total_amount);
   echo "<pre>";
 }
-
-
-/*メモ
-echo "<pre>";
-echo "1月" . $june_amount ."\n";
-echo "2月" . $february_amount . "\n";
-echo "3月" . $march_amount . "\n";
-echo "4月" . $april_amount ."\n";
-echo "5月" . $may_amount . "\n";
-echo "6月" . $june_amount . "\n";
-echo "7月" . $july_amount . "\n";
-echo "8月" . $august_amount . "\n";
-echo "9月" . $september_amount . "\n";
-echo "10月" . $october_amount . "\n";
-echo "11月" . $november_amount . "\n";
-echo "12月" . $december_amount . "\n";
-echo "<pre>";
-*/
 
 ?>

00916408585d84409528db3697ae9ca521b56acd
w-shaila <w-shaulabg.2952r-3@outlook.jp>
2022-06-29 21:29:52 +0900

    4.sort②問題５

--- src/public/sql4-5.php
+++ src/public/sql4-5.php
@@ -12,15 +12,17 @@ $amount = [];
 $month = [];
 foreach ($incomes as $income) {
   $date = explode("-", $income['accrual_date']);
-  $month = abs($date[1]);     //絶対値01⇨１
-  $amount[$month] += $income['amount'];   //$amount[1〜12月]　+=　1〜12月該当する支出のそれぞれの合計
+  $month = abs($date[1]);
+  $amount[$month] += $income['amount'];
 }
+
+arsort($amount);
 
 echo "月順にsortして月ごとの収入の合計を一覧表示";
 
-foreach ($amount as $mon => $total_amount) {   //キー(1〜１２月) =>　支出の合計　連想配列として結び付ける
+foreach ($amount as $mon => $total_amount) {
   echo "<pre>";
-  echo ($mon. "月:" . $total_amount);
+  echo ($mon . "月:" . $total_amount);
   echo "<pre>";
 }
 

ecaf699b512f6dfc6322bb11aa7e3507d2d23603
w-shaila <w-shaulabg.2952r-3@outlook.jp>
2022-07-11 15:16:37 +0900

    5.条件付きの処理、問題６

---
+++ src/public/sql5-6.php
@@ -0,0 +1,37 @@
+<?php
+$dbUserName = "root";
+$dbPassword = "password";
+$pdo = new PDO("mysql:host=mysql; dbname=tq_quest; charset=utf8", $dbUserName, $dbPassword);
+
+$sql = "SELECT * FROM spendings";
+$statement = $pdo->prepare($sql);
+$statement->execute();
+$spendings = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+$amount = [];
+$month = [];
+foreach ($spendings as $spending) {
+  $date = explode("-", $spending['accrual_date']);
+  $month = abs($date[1]);
+  $day = abs($date[2]);
+
+  if (preg_match('/5/', $day)||
+      preg_match('/15/', $day)||
+      preg_match('/25/', $day)) {
+      $amount[$month] += $spending['amount'] - 3000; 
+    continue;
+  }
+  $amount[$month] += $spending['amount'];
+}
+
+asort($amount);
+
+echo "支出の高い順にsortして月ごとの支出の合計を一覧表示。ただし、支出日に5が含まれているときだけ1500円引いてください。";
+
+foreach ($amount as $mon => $total_amount) {
+  echo "<pre>";
+  echo ($mon . "月:" . $total_amount);
+  echo "<pre>";
+}
+
+?>

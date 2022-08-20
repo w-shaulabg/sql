e63e2d8c49378db7d97ce49aeabb137eca034f5f
w-shaila <w-shaulabg.2952r-3@outlook.jp>
2022-08-20 10:56:11 +0900

    6. class内のメソッドをclassの中で呼び出す方法、問題３

---
+++ src/public/class6-3.php
@@ -0,0 +1,34 @@
+<?php
+
+class Cat
+{
+	private $name;
+	private $age;
+	
+	public function __construct($name, $age)
+	{
+		$this->name = $name;
+		$this->age = $age;
+	}
+	
+	public function introduction()
+	{
+	    $catName = $this->name();
+	    $catAge = $this->age();
+		  echo "猫の名前は" . $catName . "です". "<br/>";
+		  echo "猫の年齢は" . $catAge. "歳です";
+	}
+	
+	private function name()
+	{
+	  return $this->name;
+	}
+	
+	private function age()
+	{
+	  return $this->age;
+	}
+}
+
+$tama = new Cat("タマ", 7);
+$tama->introduction();

5e2ac405cb39efda84da0e1375398ceb1f9a98da
w-shaila <w-shaulabg.2952r-3@outlook.jp>
2022-08-20 10:10:21 +0900

    6. class内のメソッドをclassの中で呼び出す方法、問題２

---
+++ src/public/class6-1.php
@@ -0,0 +1,24 @@
+<?php
+class Calculation
+{
+	private $num;
+
+	public function __construct($num)
+	{
+		$this->num = $num;
+	}
+
+	public function output()
+	{
+	  $output = $this->addTwo();
+		return $output;
+	}
+
+	private function addTwo()
+	{
+		return $this->num + 2;
+	}
+}
+
+$calculation = new Calculation(5);
+echo $calculation->output();
